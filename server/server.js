03565d5a46d3c2b184ed87e3856de20260cb65e8
Julian Duque <julianduquej@gmail.com>
2014-10-07 10:42:19 -0500

    Add WebSockets support using Primus and ws

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-var express = require('express'),
-    http    = require('http'),
-    path    = require('path');
+var Primus        = require('primus'),
+    PrimusEmitter = require('primus-emitter'),
+    express       = require('express'),
+    http          = require('http'),
+    path          = require('path');
 
 // Express
 var app = express();
@@ -8,6 +10,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP Server
 var server = http.createServer(app);
+
+// WebSockets
+var primus = new Primus(server);
+primus.use('emitter', PrimusEmitter);
+
+primus.on('connection', function (socket) {
+  console.log(socket);
+});
 
 // Servers
 server.listen(8080);
