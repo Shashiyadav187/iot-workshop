d8dbc937ae2b119ba384c0d2d172e1bedbddaec5
Julian Duque <julianduquej@gmail.com>
2014-10-07 13:13:59 -0500

    Create a basic MQTT server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var Primus        = require('primus'),
     PrimusEmitter = require('primus-emitter'),
     express       = require('express'),
+    mqtt          = require('mqtt'),
     http          = require('http'),
     path          = require('path');
 
@@ -22,5 +23,42 @@ primus.on('connection', function (socket) {
   });
 });
 
+
+// MQTT Server
+var mqttServer = mqtt.createServer(function (client) {
+  var clients = {};
+
+  client.on('connect', function (packet) {
+    client.connack({ returnCode: 0 });
+    client.id = packet.clientId;
+    clients[client.id] = client;
+    console.log(client.id + ' connected');
+  });
+
+  client.on('pingreq', function (packet) {
+    client.pingresp();
+  });
+
+  client.on('close', function (packet) {
+    delete clients[client.id];
+    console.log(client.id + ' closed connection');
+  });
+
+  client.on('disconnect', function (packet) {
+    client.stream.end();
+    console.log(client.id + ' disconnected');
+  });
+
+
+  client.on('error', function (packet) {
+    if (!clients[client.id]) return;
+
+    delete clients[client.id];
+    client.stream.end();
+  });
+
+});
+
 // Servers
 server.listen(8080);
+mqttServer.listen(1883);
